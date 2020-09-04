4b7e8d9baa8184e0175ca00e32c04bb5c9ed8278
SImon de Bruijn <simon.debruijn@publiq.be>
2020-09-04 12:12:41 +0200

    add pub-main component

---
+++ publiq-ui/pub-main.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="main">
+    <slot />
+  </section>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="scss" scoped>
+  .main {
+    display: flex;
+    flex-direction: row;
+    position: relative;
+    z-index: 0;
+  }
+</style>
