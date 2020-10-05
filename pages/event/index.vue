cd42171dce263b4fb2f82911dba2688640c4b253
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2020-10-05 16:16:26 +0200

    Set up dummy page

---
+++ pages/event/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <pub-tags :spacing="3" />
+</template>
+
+<script>
+  import PubTags from '@/components/event/index/pub-tags';
+
+  export default {
+    components: {
+      PubTags,
+    },
+  };
+</script>
+
+<style lang="scss" scoped></style>
