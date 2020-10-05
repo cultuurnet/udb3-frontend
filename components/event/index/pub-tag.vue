29e059060e6aad5901b807feabde87dd8b403768
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2020-10-05 16:17:05 +0200

    Add tag and tags component

---
+++ components/event/index/pub-tag.vue
@@ -0,0 +1,38 @@
+<template>
+  <pub-box class="element" v-bind="$props">
+    <slot />
+  </pub-box>
+</template>
+
+<script>
+  import PubBox from './pub-box';
+  import { marginProps, paddingProps } from './pub-box.vue';
+
+  export default {
+    components: {
+      PubBox,
+    },
+    props: {
+      as: {
+        type: [String, Object],
+        default: 'li',
+      },
+      ...marginProps,
+      ...paddingProps,
+      paddingHorizontal: {
+        type: [String, Number],
+        default: 3,
+      },
+      paddingVertical: {
+        type: [String, Number],
+        default: 2,
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  .element {
+    border: 1px solid black;
+  }
+</style>
---
+++ components/event/index/pub-tags.vue
@@ -0,0 +1,31 @@
+<template>
+  <pub-stack v-bind="$props">
+    <pub-tag>a tag</pub-tag>
+    <pub-tag>another one</pub-tag>
+    <pub-tag>and one more</pub-tag>
+  </pub-stack>
+</template>
+
+<script>
+  import PubStack from './pub-stack';
+  import PubTag from './pub-tag';
+
+  import { marginProps, paddingProps } from './pub-box.vue';
+  import { spacingProps } from './pub-inline.vue';
+
+  export default {
+    components: {
+      PubStack,
+      PubTag,
+    },
+    props: {
+      as: {
+        type: [String, Object],
+        default: 'ul',
+      },
+      ...marginProps,
+      ...paddingProps,
+      ...spacingProps,
+    },
+  };
+</script>
