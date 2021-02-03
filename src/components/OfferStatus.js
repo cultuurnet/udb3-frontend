59bcab22691a9344f96f7ed039b6d7578f7ad6f2
Simon de Bruijn <simon.debruijn@publiq.be>
2021-02-03 16:30:49 +0100

    Use SelectionTable in OfferStatus

--- src/components/OfferStatus.js
+++ src/components/OfferStatus.js
@@ -1,13 +1,43 @@
 import PropTypes from 'prop-types';
+import { useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Alert, AlertVariants } from './publiq-ui/Alert';
 import { Button, ButtonVariants } from './publiq-ui/Button';
 import { Page } from './publiq-ui/Page';
 import { Spinner } from './publiq-ui/Spinner';
+import { SelectionTable } from './publiq-ui/SelectionTable';
 
 const OfferStatus = ({ offer, loading, errorMessage }) => {
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Tijdstip',
+        accessor: 'time',
+      },
+      {
+        Header: 'Status',
+        accessor: 'status',
+      },
+    ],
+    [],
+  );
+  const data = useMemo(
+    () => [
+      {
+        time: new Date().toDateString(),
+        status: 'Geannuleerd',
+      },
+      {
+        time: new Date().toDateString(),
+        status: 'Gaat door',
+      },
+    ],
+    [],
+  );
   const { t, i18n } = useTranslation();
+  const [, setSelectedOffers] = useState([]);
   const name = offer.name?.[i18n.language] ?? offer.name?.[offer.mainLanguage];
+
   return (
     <Page>
       <Page.Title>{t('offerStatus.title', { name })}</Page.Title>
@@ -19,6 +49,12 @@ const OfferStatus = ({ offer, loading, errorMessage }) => {
         ) : (
           [
             <Alert key="alert">{t('offerStatus.info')}</Alert>,
+            <SelectionTable
+              key="table"
+              columns={columns}
+              data={data}
+              onSelectionChanged={setSelectedOffers}
+            />,
             <Button
               key="button"
               variant={ButtonVariants.SUCCESS}
