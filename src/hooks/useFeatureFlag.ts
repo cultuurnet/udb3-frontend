428c80a443be28732d81e947b7802e8daff6ab74
Simon de Bruijn <simon.debruijn@publiq.be>
2021-04-19 14:50:33 +0200

    Type useFeatureFlags

--- src/hooks/useFeatureFlag.ts
+++ src/hooks/useFeatureFlag.ts
@@ -5,16 +5,18 @@ const FeatureFlags = {
   REACT_CREATE: 'react_create',
 } as const;
 
-const createCookieName = (identifier: any) => `ff_${identifier}`;
+const createCookieName = (identifier: string) => `ff_${identifier}`;
 
-const useFeatureFlag = (featureFlagName: any) => {
+const useFeatureFlag = (
+  featureFlagName: string,
+): [boolean, (value: boolean) => void] => {
   if (!featureFlagName) return [false, () => {}];
 
   const { cookies, setCookie } = useCookiesWithOptions();
 
   const cookieName = createCookieName(featureFlagName);
 
-  const set = (value: any) => setCookie(cookieName, value);
+  const set = (value: boolean) => setCookie(cookieName, value);
   const value = cookies?.[cookieName] ?? false;
 
   return [value === 'true', set];
