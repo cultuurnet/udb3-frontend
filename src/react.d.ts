78c94f93b9deac8b4fc94b5901e4a155803c7d0d
Simon de Bruijn <simon.debruijn@publiq.be>
2021-04-19 14:44:09 +0200

    Remove react.d.ts

--- src/react.d.ts
+++
@@ -1,8 +0,0 @@
-import { CSSProp } from 'styled-components';
-declare module 'react' {
-  interface Attributes {
-    className?: string;
-    forwardedAs?: string | React.ReactNode;
-    css?: CSSProp;
-  }
-}
