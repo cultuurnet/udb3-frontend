faa1e5219c6d9e6f272428f03e2fb8c20e9829df
Simon de Bruijn <simon.debruijn@publiq.be>
2021-04-19 16:19:35 +0200

    Remove all fix-me's

--- src/hooks/api/authenticated-query.ts
+++ src/hooks/api/authenticated-query.ts
@@ -24,49 +24,39 @@ const prepareKey = ({ queryKey = [], queryArguments = {} } = {}) => {
 const prepareArguments = ({
   options: {
     queryKey,
-    // @ts-expect-error ts-migrate(2525) FIXME: Initializer provides no value for this binding ele... Remove this comment to see the full error message
     queryFn = () => {},
     queryArguments = {},
     enabled = true,
     ...configuration
   } = {},
-  // @ts-expect-error ts-migrate(2525) FIXME: Initializer provides no value for this binding ele... Remove this comment to see the full error message
   isTokenPresent = false,
   headers,
 } = {}) => ({
   queryKey: prepareKey({ queryArguments, queryKey }),
-  // @ts-expect-error ts-migrate(2554) FIXME: Expected 0 arguments, but got 1.
   queryFn: () => queryFn({ ...queryArguments, headers }),
   enabled: isTokenPresent && !!enabled,
   ...configuration,
 });
 
-// @ts-expect-error ts-migrate(7006) FIXME: Parameter 'status' implicitly has an 'any' type.
 const isUnAuthorized = (status) => [401, 403].includes(status);
 
-// @ts-expect-error ts-migrate(7006) FIXME: Parameter 'results' implicitly has an 'any' type.
 const getStatusFromResults = (results) => {
-  // @ts-expect-error ts-migrate(7031) FIXME: Binding element 'status' implicitly has an 'any' t... Remove this comment to see the full error message
   if (results.some(({ status }) => status === QueryStatus.ERROR)) {
     return {
       status: QueryStatus.ERROR,
       error: results
         .map(({ error }) => error)
-        // @ts-expect-error ts-migrate(7031) FIXME: Binding element 'error' implicitly has an 'any' ty... Remove this comment to see the full error message
         .filter((error) => error !== undefined),
     };
   }
 
-  // @ts-expect-error ts-migrate(7031) FIXME: Binding element 'status' implicitly has an 'any' t... Remove this comment to see the full error message
   if (results.every(({ status }) => status === QueryStatus.SUCCESS)) {
     return { status: QueryStatus.SUCCESS };
   }
-  // @ts-expect-error ts-migrate(7031) FIXME: Binding element 'status' implicitly has an 'any' t... Remove this comment to see the full error message
   if (results.some(({ status }) => status === QueryStatus.LOADING)) {
     return { status: QueryStatus.LOADING };
   }
   if (results.some(({ status }) => status === QueryStatus.IDLE)) {
-    // @ts-expect-error ts-migrate(7031) FIXME: Binding element 'status' implicitly has an 'any' t... Remove this comment to see the full error message
     return { status: QueryStatus.IDLE };
   }
 };
@@ -76,11 +66,9 @@ const prefetchAuthenticatedQueries = async ({
   queryClient,
   options: rawOptions = [],
 }) => {
-  // @ts-expect-error ts-migrate(7031) FIXME: Binding element 'req' implicitly has an 'any' type... Remove this comment to see the full error message
   const cookies = new Cookies(req?.headers?.cookie);
   const headers = createHeaders(cookies.get('token'));
 
-  // @ts-expect-error ts-migrate(2554) FIXME: Expected 2 arguments, but got 1.
   const preparedArguments = rawOptions.map((options) =>
     prepareArguments({
       options,
@@ -100,15 +88,12 @@ const prefetchAuthenticatedQueries = async ({
   );
 };
 
-// @ts-expect-error ts-migrate(7031) FIXME: Binding element 'req' implicitly has an 'any' type... Remove this comment to see the full error message
 const prefetchAuthenticatedQuery = async ({ req, queryClient, ...options }) => {
   const cookies = new Cookies(req?.headers?.cookie);
-  // @ts-expect-error ts-migrate(2554) FIXME: Expected 2 arguments, but got 1.
   const headers = createHeaders(cookies.get('token'));
 
   const { queryKey, queryFn } = prepareArguments({
     options,
-    // @ts-expect-error ts-migrate(2741) FIXME: Property 'queryKey' is missing in type '{ [x: stri... Remove this comment to see the full error message
     isTokenPresent: isTokenValid(cookies.get('token')),
     headers,
   });
@@ -133,10 +118,8 @@ const useAuthenticatedMutation = ({
   const innerMutationFn = useCallback(async (variables) => {
     const response = await mutationFn({ ...variables, headers });
 
-    // @ts-expect-error ts-migrate(2554) FIXME: Expected 0 arguments, but got 1.
     if (isUnAuthorized(response?.status)) {
       removeAuthenticationCookies();
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'status' does not exist on type 'void'.
       router.push('/login');
     }
 
@@ -146,7 +129,6 @@ const useAuthenticatedMutation = ({
       return '';
     }
     return JSON.parse(result);
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'text' does not exist on type 'void'.
   }, []);
 
   return useMutation(innerMutationFn, configuration);
@@ -164,23 +146,18 @@ const useAuthenticatedMutations = ({
   const innerMutationFn = useCallback(async (variables) => {
     const responses = await mutationFns({ ...variables, headers });
 
-    // @ts-expect-error ts-migrate(2349) FIXME: This expression is not callable.
     if (responses.some((response) => isUnAuthorized(response.status))) {
       removeAuthenticationCookies();
-      // @ts-expect-error ts-migrate(7006) FIXME: Parameter 'response' implicitly has an 'any' type.
       router.push('/login');
     } else if (responses.some((response) => response.type === 'ERROR')) {
       const errorMessages = responses
         .filter((response) => response.type === 'ERROR')
-        // @ts-expect-error ts-migrate(7006) FIXME: Parameter 'response' implicitly has an 'any' type.
         .map((response) => response.title)
         .join(', ');
       throw new Error(errorMessages);
     }
 
-    // @ts-expect-error ts-migrate(7006) FIXME: Parameter 'response' implicitly has an 'any' type.
     return Promise.all(
-      // @ts-expect-error ts-migrate(7006) FIXME: Parameter 'response' implicitly has an 'any' type.
       responses.map(async (response) => {
         const result = await response.text();
 
@@ -191,7 +168,6 @@ const useAuthenticatedMutations = ({
         return JSON.parse(result);
       }),
     );
-    // @ts-expect-error ts-migrate(7006) FIXME: Parameter 'response' implicitly has an 'any' type.
   }, []);
 
   return useMutation(innerMutationFn, configuration);
@@ -204,14 +180,11 @@ const useAuthenticatedQuery = (options = {}) => {
 
   const { asPath, ...router } = useRouter();
 
-  // @ts-expect-error ts-migrate(2339) FIXME: Property 'req' does not exist on type '{}'.
   const headers = useHeaders();
-  // @ts-expect-error ts-migrate(2339) FIXME: Property 'queryClient' does not exist on type '{}'... Remove this comment to see the full error message
   const { cookies, removeAuthenticationCookies } = useCookiesWithOptions([
     'token',
   ]);
 
-  // @ts-expect-error ts-migrate(2345) FIXME: Argument of type '{}' is not assignable to paramet... Remove this comment to see the full error message
   const preparedArguments = prepareArguments({
     options,
     isTokenPresent: isTokenValid(cookies.token),
@@ -221,7 +194,6 @@ const useAuthenticatedQuery = (options = {}) => {
   const result = useQuery(preparedArguments);
 
   if (isUnAuthorized(result?.error?.status)) {
-    // @ts-expect-error ts-migrate(2741) FIXME: Property 'queryKey' is missing in type '{}' but re... Remove this comment to see the full error message
     if (!asPath.startsWith('/login') && asPath !== '/[...params]') {
       removeAuthenticationCookies();
       router.push('/login');
@@ -231,7 +203,6 @@ const useAuthenticatedQuery = (options = {}) => {
   return result;
 };
 
-// @ts-expect-error ts-migrate(2571) FIXME: Object is of type 'unknown'.
 const useAuthenticatedQueries = ({
   req,
   queryClient,
@@ -245,7 +216,6 @@ const useAuthenticatedQueries = ({
     });
   }
 
-  // @ts-expect-error ts-migrate(7031) FIXME: Binding element 'req' implicitly has an 'any' type... Remove this comment to see the full error message
   const { asPath, ...router } = useRouter();
 
   const headers = useHeaders();
@@ -272,7 +242,6 @@ const useAuthenticatedQueries = ({
 
   return {
     data: results.map(({ data }) => data).filter((data) => data !== undefined),
-    // @ts-expect-error ts-migrate(2571) FIXME: Object is of type 'unknown'.
     ...getStatusFromResults(results),
   };
 };
