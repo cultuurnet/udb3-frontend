9f473fe03e52099a5dbdc26b88d193f21e17b30c
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-04-20 17:25:31 +0200

    Add types for isValidToken

--- src/utils/isTokenValid.ts
+++ src/utils/isTokenValid.ts
@@ -1,19 +1,25 @@
 import base64 from 'base-64';
 
-const decode = (value) =>
+type Token = {
+  header?: string;
+  payload?: { exp?: number };
+  signature?: string;
+};
+
+const decode = (value: string) =>
   base64.decode(value.replace(/-/g, '+').replace(/_/g, '/'));
 
-const isTokenValid = (token) => {
+const isTokenValid = (token: string) => {
   if (!token) return false;
   const keys = ['header', 'payload', 'signature'];
 
   const parts = token.split('.');
   if (parts.length !== 3) return false;
 
-  let decodedToken;
+  let decodedToken: Token;
 
   try {
-    decodedToken = parts.reduce((token, part, index) => {
+    decodedToken = parts.reduce<Token>((token, part, index) => {
       if (!part) throw new Error('');
       const data = index < 2 ? JSON.parse(decode(part)) : part;
       return { ...token, [keys[index]]: data };
