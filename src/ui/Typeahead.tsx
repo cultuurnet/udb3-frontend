5668194b34300aac53a3d855323e10a29584711e
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-04-20 15:21:56 +0200

    Initial Typeahead typing

--- src/ui/Typeahead.tsx
+++ src/ui/Typeahead.tsx
@@ -1,14 +1,28 @@
-import PropTypes from 'prop-types';
-// @ts-expect-error ts-migrate(7016) FIXME: Could not find a declaration file for module 'reac... Remove this comment to see the full error message
 import { AsyncTypeahead as BootstrapTypeahead } from 'react-bootstrap-typeahead';
 import { getValueFromTheme } from './theme';
-// @ts-expect-error ts-migrate(6142) FIXME: Module './Box' was resolved to '/Users/simondebrui... Remove this comment to see the full error message
-import { Box, boxPropTypes, getBoxProps } from './Box';
-import { forwardRef } from 'react';
+
+import { Box, getBoxProps } from './Box';
+import type { BoxProps } from './Box';
+
+import { forwardRef, Ref } from 'react';
 
 const getValue = getValueFromTheme('typeahead');
 
-const Typeahead = forwardRef(
+type Props = BoxProps & {
+  id: string;
+  options: Array<unknown>;
+  labelKey: () => void;
+  disabled: boolean;
+  placeholder: string;
+  emptyLabel: string;
+  minLength: number;
+  className: string;
+  onInputChange: () => void;
+  onSearch: () => void;
+  onChange: () => void;
+};
+
+const Typeahead = forwardRef<Ref<HTMLDivElement>, Props>(
   (
     {
       id,
@@ -19,29 +33,19 @@ const Typeahead = forwardRef(
       emptyLabel,
       minLength,
       className,
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'id' does not exist on type '{ children?:... Remove this comment to see the full error message
       onInputChange,
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'labelKey' does not exist on type '{ chil... Remove this comment to see the full error message
       onSearch,
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'disabled' does not exist on type '{ chil... Remove this comment to see the full error message
       onChange,
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'placeholder' does not exist on type '{ c... Remove this comment to see the full error message
       ...props
     },
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'emptyLabel' does not exist on type '{ ch... Remove this comment to see the full error message
     ref,
   ) => {
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'minLength' does not exist on type '{ chi... Remove this comment to see the full error message
     return (
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'className' does not exist on type '{ chi... Remove this comment to see the full error message
       <Box
-        // @ts-expect-error ts-migrate(2339) FIXME: Property 'onInputChange' does not exist on type '{... Remove this comment to see the full error message
         forwardedAs={BootstrapTypeahead}
-        // @ts-expect-error ts-migrate(2339) FIXME: Property 'onChange' does not exist on type '{ chil... Remove this comment to see the full error message
         id={id}
         options={options}
         labelKey={labelKey}
-        // @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
         isLoading={false}
         disabled={disabled}
         className={className}
@@ -74,37 +78,16 @@ const Typeahead = forwardRef(
   },
 );
 
-const typeaheadPropTypes = {
-  id: PropTypes.string.isRequired,
-  options: PropTypes.array,
-  labelKey: PropTypes.func,
-  disabled: PropTypes.bool,
-  placeholder: PropTypes.string,
-  emptyLabel: PropTypes.string,
-  minLength: PropTypes.number,
-  className: PropTypes.string,
-  onInputChange: PropTypes.func,
-  onSearch: PropTypes.func,
-  onChange: PropTypes.func,
-};
-
-Typeahead.propTypes = {
-  ...boxPropTypes,
-  ...typeaheadPropTypes,
-};
-
 const typeaheadDefaultProps = {
   options: [],
-  labelKey: (item) => item,
+  labelKey: () => {},
   onSearch: async () => {},
-  // @ts-expect-error ts-migrate(7006) FIXME: Parameter 'item' implicitly has an 'any' type.
   disabled: false,
   minLength: 3,
 };
 
 Typeahead.defaultProps = {
-  // @ts-expect-error ts-migrate(2559) FIXME: Type '{ options: never[]; labelKey: (item: any) =>... Remove this comment to see the full error message
   ...typeaheadDefaultProps,
 };
 
-export { Typeahead, typeaheadPropTypes, typeaheadDefaultProps };
+export { Typeahead, typeaheadDefaultProps };
--- src/ui/TypeaheadWithLabel.tsx
+++ src/ui/TypeaheadWithLabel.tsx
@@ -3,9 +3,7 @@ import {
   Typeahead,
   typeaheadDefaultProps,
   typeaheadPropTypes,
-  // @ts-expect-error ts-migrate(6142) FIXME: Module './Typeahead' was resolved to '/Users/simon... Remove this comment to see the full error message
 } from './Typeahead';
-// @ts-expect-error ts-migrate(6142) FIXME: Module './Stack' was resolved to '/Users/simondebr... Remove this comment to see the full error message
 import { getStackProps, Stack, stackPropTypes } from './Stack';
 import { forwardRef } from 'react';
 
@@ -20,36 +18,24 @@ const TypeaheadWithLabel = forwardRef(
       placeholder,
       emptyLabel,
       minLength,
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'id' does not exist on type '{ children?:... Remove this comment to see the full error message
       className,
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'label' does not exist on type '{ childre... Remove this comment to see the full error message
       onInputChange,
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'labelKey' does not exist on type '{ chil... Remove this comment to see the full error message
       onSearch,
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'disabled' does not exist on type '{ chil... Remove this comment to see the full error message
       onChange,
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'placeholder' does not exist on type '{ c... Remove this comment to see the full error message
       ...props
     },
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'emptyLabel' does not exist on type '{ ch... Remove this comment to see the full error message
     ref,
   ) => {
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'minLength' does not exist on type '{ chi... Remove this comment to see the full error message
     return (
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'className' does not exist on type '{ chi... Remove this comment to see the full error message
       <Stack {...getStackProps(props)}>
-        {/* @ts-expect-error ts-migrate(2339) FIXME: Property 'onSearch' does not exist on type '{ chil... Remove this comment to see the full error message */}
         <Label htmlFor={id} variant={LabelVariants.BOLD}>
           {label}
         </Label>
-        {/* @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message */}
         <Typeahead
           id={id}
-          // @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
           options={options}
           labelKey={labelKey}
           disabled={disabled}
-          // @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
           emptyLabel={emptyLabel}
           minLength={minLength}
           placeholder={placeholder}
