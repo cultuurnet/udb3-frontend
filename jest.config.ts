a997c67870f4026e02483297c6fec7e8ae61ad93
Simon de Bruijn <simon.debruijn@publiq.be>
2021-04-23 15:38:05 +0200

    Make jest runnable

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,14 @@
-const JsConfigPathsMapper = require('jsconfig-paths-jest-mapper');
+import JsConfigPathsMapper from 'jsconfig-paths-jest-mapper';
 const jsconfigpaths = new JsConfigPathsMapper();
 
 module.exports = {
-  moduleFileExtensions: ['js', 'json'],
+  moduleFileExtensions: ['js', 'ts', 'tsx', 'json'],
   transform: {
     '^.+\\.js$': 'babel-jest',
   },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.js'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '<rootDir>/src/**/*.tsx'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     ...jsconfigpaths,
