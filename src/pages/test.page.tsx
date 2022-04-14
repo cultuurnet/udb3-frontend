0b55d57d216dfdb1b4d2ff1ebeff10676e074b38
simon-debruijn <simon.debruijn@hotmail.com>
2022-04-14 10:44:32 +0200

    Add server side api call to test example

--- src/pages/test.page.tsx
+++ src/pages/test.page.tsx
@@ -1,4 +1,6 @@
+import { NextApiRequest } from 'next';
 import { useEffect } from 'react';
+import { dehydrate } from 'react-query/types/hydration';
 
 import {
   AuthenticatedQueryFunctionContext,
@@ -7,32 +9,33 @@ import {
 import { Headers } from '@/hooks/api/types/Headers';
 import { Event } from '@/types/Event';
 import { fetchFromApi, isErrorObject } from '@/utils/fetchFromApi';
+import { getApplicationServerSideProps } from '@/utils/getApplicationServerSideProps';
 
 type GetEventByIdArguments = {
   headers: Headers;
   id: string;
 };
 
+const getEventById = async ({
+  queryKey,
+  headers,
+}: AuthenticatedQueryFunctionContext) => {
+  const [key, { id }] = queryKey;
+  const res = await fetchFromApi({
+    path: `/event/${id.toString()}`,
+    options: {
+      headers,
+    },
+  });
+  if (isErrorObject(res)) {
+    // eslint-disable-next-line no-console
+    return console.error(res);
+  }
+  return await res.json();
+};
+
 const Test = () => {
   const id = 'a9e9cdce-2e3c-4b17-9c6b-a7eb445252d1';
-
-  const getEventById = async ({
-    queryKey,
-    headers,
-  }: AuthenticatedQueryFunctionContext) => {
-    const [key, { id }] = queryKey;
-    const res = await fetchFromApi({
-      path: `/event/${id.toString()}`,
-      options: {
-        headers,
-      },
-    });
-    if (isErrorObject(res)) {
-      // eslint-disable-next-line no-console
-      return console.error(res);
-    }
-    return await res.json();
-  };
 
   const query = useAuthenticatedQuery<Event | null>({
     queryKey: ['events'],
@@ -48,5 +51,32 @@ const Test = () => {
 
   return null;
 };
+
+export const getServerSideProps = getApplicationServerSideProps(
+  async ({
+    req,
+    cookies,
+    queryClient,
+  }: {
+    req: NextApiRequest;
+    cookies: any;
+    queryClient: any;
+  }) => {
+    useAuthenticatedQuery<Event | null>({
+      req,
+      queryClient,
+      queryKey: ['events'],
+      queryFn: getEventById,
+      queryArguments: { id: 'a9e9cdce-2e3c-4b17-9c6b-a7eb445252d1' },
+    });
+
+    return {
+      props: {
+        dehydratedState: dehydrate(queryClient),
+        cookies,
+      },
+    };
+  },
+);
 
 export default Test;
