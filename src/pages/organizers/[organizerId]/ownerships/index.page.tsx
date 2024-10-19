9eabf144d4366dbe8beddb8780ef4b25e5f8505f
Emma Fabre <ehtnam6@gmail.com>
2024-10-19 12:11:24 +0200

    Add dummy buttons to ownerships page

--- src/pages/organizers/[organizerId]/ownerships/index.page.tsx
+++ src/pages/organizers/[organizerId]/ownerships/index.page.tsx
@@ -1,17 +1,34 @@
 import { Page } from '@/ui/Page';
 import { getApplicationServerSideProps } from '@/utils/getApplicationServerSideProps';
+import { Stack } from '@/ui/Stack';
+import { Inline } from '@/ui/Inline';
+import { Button, ButtonVariants } from '@/ui/Button';
 
 const Ownership = () => {
   return (
     <Page>
       <Page.Title>Hello world</Page.Title>
       <Page.Content>
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eos
-          nisi maiores. Doloribus molestias magnam facilis! Eum rerum ea fugit
-          excepturi doloribus, assumenda quod vitae magni tempore voluptatum
-          adipisci aliquam.
-        </div>
+        <Inline
+          display={'grid'}
+          css={`
+            grid-template-columns: 3fr 1fr;
+            gap: 1rem;
+          `}
+        >
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eos
+            nisi maiores. Doloribus molestias magnam facilis! Eum rerum ea fugit
+            excepturi doloribus, assumenda quod vitae magni tempore voluptatum
+            adipisci aliquam.
+          </div>
+          <Stack spacing={3}>
+            <Button>Nieuwe beheerder toevoegen</Button>
+            <Button variant={ButtonVariants.SECONDARY}>
+              Terug naar orgonisatiepagina
+            </Button>
+          </Stack>
+        </Inline>
       </Page.Content>
     </Page>
   );
