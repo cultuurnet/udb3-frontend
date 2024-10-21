35ed8bbc0bf22e5c3945c9039ddca16f5738758a
vhande <78013271+vhande@users.noreply.github.com>
2024-10-21 09:21:58 +0200

    Add query

---
+++ src/hooks/api/ownerships.ts
@@ -0,0 +1,61 @@
+import { UseQueryOptions } from 'react-query';
+
+import { Values } from '@/types/Values';
+import { fetchFromApi, isErrorObject } from '@/utils/fetchFromApi';
+
+import {
+  ServerSideQueryOptions,
+  useAuthenticatedQuery,
+} from './authenticated-query';
+
+export type OwnershipRequest = {
+  id: string;
+  itemId: string;
+  ownerId: string;
+  requesterId: string;
+  state: RequestState;
+};
+
+export const RequestState = {
+  APPROVED: 'approved',
+  REQUESTED: 'requested',
+} as const;
+
+type RequestState = Values<typeof RequestState>;
+
+const getOwnershipRequests = async ({ headers, organizerId }) => {
+  const res = await fetchFromApi({
+    path: '/ownerships/',
+    searchParams: {
+      itemId: organizerId,
+    },
+    options: {
+      headers,
+    },
+  });
+  if (isErrorObject(res)) {
+    // eslint-disable-next-line no-console
+    return console.error(res);
+  }
+  return await res.json();
+};
+
+type UseGetOwnershipRequestsArguments = ServerSideQueryOptions & {
+  organizerId: string;
+};
+
+const useGetOwnershipRequestsQuery = (
+  { req, queryClient, organizerId }: UseGetOwnershipRequestsArguments,
+  configuration: UseQueryOptions = {},
+) =>
+  useAuthenticatedQuery<OwnershipRequest[]>({
+    req,
+    queryClient,
+    queryKey: ['ownership-requests'],
+    queryFn: getOwnershipRequests,
+    queryArguments: { organizerId },
+    refetchOnWindowFocus: false,
+    ...configuration,
+  });
+
+export { useGetOwnershipRequestsQuery };
