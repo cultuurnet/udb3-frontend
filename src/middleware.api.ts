8862c048dfc8eedb5b3ea525f0afa88cc06c07f5
simon-debruijn <simon.debruijn@hotmail.com>
2024-10-29 14:24:53 +0100

    Remove params

--- src/middleware.api.ts
+++ src/middleware.api.ts
@@ -54,7 +54,9 @@ export const middleware = async (request: NextRequest) => {
     const isOwnershipEnabled =
       process.env.NEXT_PUBLIC_OWNERSHIP_ENABLED === 'true';
 
-    const redirectUrl = new URL(request.nextUrl);
+    const redirectUrl = new URL(request.url);
+    // remove the path variables from nextjs routing
+    redirectUrl.searchParams.delete('params');
 
     if (
       isOwnershipEnabled &&
