f35e791050a8eedb43032e13612b8b43f4a92e06
simon-debruijn <simon.debruijn@hotmail.com>
2024-10-29 17:04:16 +0100

    Add withTimezone helper

---
+++ src/test/e2e/withTimezone.ts
@@ -0,0 +1,11 @@
+import { type Page, type Browser } from '@playwright/test';
+
+export async function withTimezone(
+  ctx: { browser: Browser; timezoneId: string },
+  testFn: (page: Page) => Promise<void>,
+) {
+  const context = await ctx.browser.newContext({ timezoneId: ctx.timezoneId });
+  const page = await context.newPage();
+  await testFn(page);
+  await context.close();
+}
