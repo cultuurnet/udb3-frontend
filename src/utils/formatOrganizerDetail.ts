fb3ebd30720cb08d5ee40cce827d6313e572c04f
vhande <78013271+vhande@users.noreply.github.com>
2024-11-20 17:37:30 +0100

    Add utils formatOrganizerDetail

---
+++ src/utils/formatOrganizerDetail.ts
@@ -0,0 +1,32 @@
+import { getAddress } from '@/pages/create/OfferForm';
+import { Organizer } from '@/types/Organizer';
+
+import { SupportedLanguage } from '../i18n';
+
+const parseAddress = (
+  offer: Organizer,
+  language: SupportedLanguage,
+  mainLanguage: SupportedLanguage,
+) => {
+  const { addressLocality, postalCode, streetAddress } = getAddress(
+    offer.address,
+    language,
+    mainLanguage,
+  );
+
+  return `${streetAddress}\n${postalCode} ${addressLocality}`;
+};
+
+const formatEmailAndPhone = ({ email, phone }: Organizer['contactPoint']) => {
+  const contactDetails = [...email, ...phone];
+  if (contactDetails.length === 0) {
+    return null;
+  }
+  return contactDetails.join('\n');
+};
+
+const replaceHTMLTags = (text: string) => {
+  return text.replace(/<\/?[^>]+(>|$)/g, '');
+};
+
+export { formatEmailAndPhone, parseAddress, replaceHTMLTags };
