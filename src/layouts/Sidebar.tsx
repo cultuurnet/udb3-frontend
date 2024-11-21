61b30bde125da2511b45104e9b5f37bb36867e7a
simon-debruijn <simon.debruijn@hotmail.com>
2024-11-21 16:30:41 +0100

    Add ownerships link to dashboard

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -578,6 +578,12 @@ const Sidebar = () => {
         children: t('menu.organizations'),
       },
       {
+        permission: PermissionTypes.ORGANISATIES_BEHEREN,
+        href: '/manage/ownerships',
+        iconName: Icons.SLIDE_SHARE,
+        children: t('menu.ownerships'),
+      },
+      {
         permission: PermissionTypes.PRODUCTIES_AANMAKEN,
         href: '/manage/productions',
         iconName: Icons.LAYER_GROUP,
