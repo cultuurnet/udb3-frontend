648a5502836d420cca26646f7d562ebc83a605ac
Emma Fabre <ehtnam6@gmail.com>
2025-01-29 12:44:46 +0100

    Render both empty state and table with creator when no ownerships

--- src/pages/organizers/[organizerId]/ownerships/OwnershipsTable.tsx
+++ src/pages/organizers/[organizerId]/ownerships/OwnershipsTable.tsx
@@ -14,6 +14,7 @@ import { Stack } from '@/ui/Stack';
 import { Text } from '@/ui/Text';
 import { colors, getValueFromTheme } from '@/ui/theme';
 import { Title } from '@/ui/Title';
+import { Alert, AlertVariants } from '@/ui/Alert';
 
 const getGlobalValue = getValueFromTheme('global');
 
@@ -95,82 +96,85 @@ export const OwnershipsTable = ({
     [requests],
   );
 
-  if (requests.length === 0) {
-    return <Text>{t('organizers.ownerships.table.no_results')}</Text>;
-  }
-
   return (
-    <Stack
-      role="table"
-      flex={1}
-      padding={4}
-      marginBottom={5}
-      borderRadius="0.5rem"
-      backgroundColor="white"
-      maxWidth="85rem"
-      css={`
-        box-shadow: ${getGlobalValue('boxShadow.medium')};
-      `}
-    >
-      <Inline
-        paddingBottom={3}
+    <>
+      <Stack
+        role="table"
+        flex={1}
+        padding={4}
+        marginBottom={5}
+        borderRadius="0.5rem"
+        backgroundColor="white"
+        maxWidth="85rem"
         css={`
-          display: grid;
-          grid-template-columns: repeat(${shouldShowItemId ? 3 : 2}, 1fr);
-          border-bottom: 1px solid ${grey3};
+          box-shadow: ${getGlobalValue('boxShadow.medium')};
         `}
       >
-        <Title size={3}>{t('organizers.ownerships.table.user')}</Title>
-        {shouldShowItemId && <Title size={3}>item id</Title>}
-        {hasActions && (
-          <Title size={3} justifyContent="flex-end">
-            {t('organizers.ownerships.table.actions.title')}
-          </Title>
-        )}
-      </Inline>
-      <List paddingY={3}>
-        {creator?.email && <List.Item>{creator.email}</List.Item>}
-        {requests.map((request) => (
-          <Inline
-            key={request.id}
-            role="row"
-            alignItems="center"
-            paddingY={3}
-            display="grid"
-            css={`
-              grid-template-columns: repeat(${shouldShowItemId ? 3 : 2}, 1fr);
+        <Inline
+          paddingBottom={3}
+          css={`
+            display: grid;
+            grid-template-columns: repeat(${shouldShowItemId ? 3 : 2}, 1fr);
+            border-bottom: 1px solid ${grey3};
+          `}
+        >
+          <Title size={3}>{t('organizers.ownerships.table.user')}</Title>
+          {shouldShowItemId && <Title size={3}>item id</Title>}
+          {hasActions && (
+            <Title size={3} justifyContent="flex-end">
+              {t('organizers.ownerships.table.actions.title')}
+            </Title>
+          )}
+        </Inline>
+        <List paddingY={3}>
+          {creator?.email && <List.Item>{creator.email}</List.Item>}
+          {requests.map((request) => (
+            <Inline
+              key={request.id}
+              role="row"
+              alignItems="center"
+              paddingY={3}
+              display="grid"
+              css={`
+                grid-template-columns: repeat(${shouldShowItemId ? 3 : 2}, 1fr);
 
-              &:not(:last-child) {
-                border-bottom: 1px solid ${grey3};
-              }
-            `}
-          >
-            <List.Item>
-              <Stack>
-                {shouldShowOwnerId && <Text>{request.ownerId}</Text>}
-                <Text>{request.ownerEmail}</Text>
-              </Stack>
-            </List.Item>
-            {shouldShowItemId && (
+                &:not(:last-child) {
+                  border-bottom: 1px solid ${grey3};
+                }
+              `}
+            >
               <List.Item>
                 <Stack>
-                  <Text>{request.itemId}</Text>
+                  {shouldShowOwnerId && <Text>{request.ownerId}</Text>}
+                  <Text>{request.ownerEmail}</Text>
                 </Stack>
               </List.Item>
-            )}
-            {hasActions && (
-              <List.Item justifyContent="flex-end">
-                <Actions
-                  request={request}
-                  onDelete={onDelete}
-                  onApprove={onApprove}
-                  onReject={onReject}
-                />
-              </List.Item>
-            )}
-          </Inline>
-        ))}
-      </List>
-    </Stack>
+              {shouldShowItemId && (
+                <List.Item>
+                  <Stack>
+                    <Text>{request.itemId}</Text>
+                  </Stack>
+                </List.Item>
+              )}
+              {hasActions && (
+                <List.Item justifyContent="flex-end">
+                  <Actions
+                    request={request}
+                    onDelete={onDelete}
+                    onApprove={onApprove}
+                    onReject={onReject}
+                  />
+                </List.Item>
+              )}
+            </Inline>
+          ))}
+        </List>
+      </Stack>
+      {requests.length === 0 && (
+        <Alert variant={AlertVariants.WARNING} fullWidth>
+          {t('organizers.ownerships.table.no_results')}
+        </Alert>
+      )}
+    </>
   );
 };
