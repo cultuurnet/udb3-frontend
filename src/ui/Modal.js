d03daec5031c9dbac2dbaa2914bfe7aecdbb929c
simon-debruijn <simon.debruijn@hotmail.com>
2025-02-21 15:18:34 +0100

    Exclude Modal changes

---
+++ src/ui/Modal.js
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+
+import { ContentModal } from './Modal/ContentModal';
+import { QuestionModal } from './Modal/QuestionModal';
+
+const ModalVariants = {
+  QUESTION: 'question',
+  CONTENT: 'content',
+};
+
+const ModalSizes = {
+  SM: 'sm',
+  MD: 'md',
+  LG: 'lg',
+  XL: 'xl',
+};
+
+const Components = {
+  [ModalVariants.QUESTION]: QuestionModal,
+  [ModalVariants.CONTENT]: ContentModal,
+};
+
+const Modal = ({ variant, ...props }) => {
+  const ModalVariant = Components[variant];
+  if (!ModalVariant) return null;
+  return <ModalVariant {...props} />;
+};
+
+Modal.propTypes = {
+  variant: PropTypes.oneOf(Object.values(ModalVariants)),
+  size: PropTypes.oneOf(Object.values(ModalSizes)),
+  className: PropTypes.string,
+  visible: PropTypes.bool,
+  title: PropTypes.string,
+  onShow: PropTypes.func,
+  scrollable: PropTypes.bool,
+  onClose: PropTypes.func,
+  children: PropTypes.node,
+  confirmTitle: PropTypes.string,
+  cancelTitle: PropTypes.string,
+  onConfirm: PropTypes.func,
+  confirmButtonDisabled: PropTypes.bool,
+};
+
+Modal.defaultProps = {
+  variant: ModalVariants.CONTENT,
+};
+
+export { Modal, ModalSizes, ModalVariants };
