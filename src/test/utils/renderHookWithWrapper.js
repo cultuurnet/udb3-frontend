13b279dace8f4a3c020bbad303705c3123871b75
simon-debruijn <simon.debruijn@hotmail.com>
2025-03-27 15:44:14 +0100

    Fix hooks wrapper

--- src/test/utils/renderHookWithWrapper.js
+++ src/test/utils/renderHookWithWrapper.js
@@ -1,7 +1,11 @@
 import { renderHook } from '@testing-library/react-hooks';
+import Cookies from 'universal-cookie'
 
 import App from '@/pages/_app.page';
 
-const renderHookWithWrapper = (hook) => renderHook(hook, { wrapper: App });
+const renderHookWithWrapper = (hook) => renderHook(hook, {
+    wrapper: (props) =>
+        App({ ...props, Component: { universalCookies: new Cookies() } }),
+});
 
 export { renderHookWithWrapper };
