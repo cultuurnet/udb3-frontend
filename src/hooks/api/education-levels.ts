793ef055fa1abbc78e231915d16258ee77f66583
Emma Fabre <ehtnam6@gmail.com>
2025-04-09 11:49:12 +0200

    Fixed encoded characters in dummy levels

--- src/hooks/api/education-levels.ts
+++ src/hooks/api/education-levels.ts
@@ -4,66 +4,66 @@ import type { Node } from 'react-checkbox-tree';
 const dummyData: Node[] = [
   {
     label: 'Basisonderwijs',
-    value: 'cultuurkuur+AF8-basisonderwijs',
+    value: 'cultuurkuur_basisonderwijs',
     children: [
       {
         label: 'Gewoon basisonderwijs',
-        value: 'cultuurkuur+AF8-Gewoon+AC0-basisonderwijs',
+        value: 'cultuurkuur_Gewoon-basisonderwijs',
         children: [
           {
             label: 'Gewoon kleuteronderwijs',
-            value: 'cultuurkuur+AF8-Gewoon+AC0-kleuteronderwijs',
+            value: 'cultuurkuur_Gewoon-kleuteronderwijs',
             children: [
               {
-                label: 'Kleuter (2+AC0-3 jaar)',
-                value: 'cultuurkuur+AF8-Kleuter+AC0-2+AC0-3+AC0-jaar',
+                label: 'Kleuter (2-3 jaar)',
+                value: 'cultuurkuur_Kleuter-2-3-jaar',
               },
               {
-                label: 'Kleuter (3+AC0-4 jaar)',
-                value: 'cultuurkuur+AF8-Kleuter+AC0-3+AC0-4+AC0-jaar',
+                label: 'Kleuter (3-4 jaar)',
+                value: 'cultuurkuur_Kleuter-3-4-jaar',
               },
               {
-                label: 'Kleuter (4+AC0-5 jaar)',
-                value: 'cultuurkuur+AF8-Kleuter+AC0-4+AC0-5+AC0-jaar',
+                label: 'Kleuter (4-5 jaar)',
+                value: 'cultuurkuur_Kleuter-4-5-jaar',
               },
             ],
           },
           {
             label: 'Gewoon lager onderwijs',
-            value: 'cultuurkuur+AF8-Gewoon+AC0-lager+AC0-onderwijs',
+            value: 'cultuurkuur_Gewoon-lager-onderwijs',
             children: [
               {
                 label: 'Eerste graad',
-                value: 'cultuurkuur+AF8-1ste+AC0-graad',
+                value: 'cultuurkuur_1ste-graad',
               },
               {
                 label: 'Tweede graad',
-                value: 'cultuurkuur+AF8-2de+AC0-graad',
+                value: 'cultuurkuur_2de-graad',
               },
               {
                 label: 'Derde graad',
-                value: 'cultuurkuur+AF8-3de+AC0-graad',
+                value: 'cultuurkuur_3de-graad',
               },
             ],
           },
           {
             label: 'Onthaalonderwijs voor anderstalige nieuwkomers (OKAN)',
             value:
-              'cultuurkuur+AF8-Onthaalonderwijs+AC0-voor+AC0-anderstalige+AC0-nieuwkomers (OKAN)',
+              'cultuurkuur_Onthaalonderwijs-voor-anderstalige-nieuwkomers (OKAN)',
           },
         ],
       },
       {
         label: 'Buitengewoon basisonderwijs',
-        value: 'cultuurkuur+AF8-Buitengewoon+AC0-basisonderwijs',
+        value: 'cultuurkuur_Buitengewoon-basisonderwijs',
         children: [
           {
             label: 'Buitengewoon kleuteronderwijs',
-            value: 'cultuurkuur+AF8-Buitengewoon+AC0-kleuteronderwijs',
+            value: 'cultuurkuur_Buitengewoon-kleuteronderwijs',
           },
           {
             label: 'Buitengewoon lager onderwijs',
-            value: 'cultuurkuur+AF8-Buitengewoon+AC0-lager+AC0-onderwijs',
+            value: 'cultuurkuur_Buitengewoon-lager-onderwijs',
           },
         ],
       },
@@ -71,112 +71,110 @@ const dummyData: Node[] = [
   },
   {
     label: 'Secundair onderwijs',
-    value: 'cultuurkuur+AF8-Secundair+AC0-onderwijs',
+    value: 'cultuurkuur_Secundair-onderwijs',
     children: [
       {
         label: 'Voltijds gewoon secundair onderwijs',
-        value:
-          'cultuurkuur+AF8-Voltijds+AC0-gewoon+AC0-secundair+AC0-onderwijs',
+        value: 'cultuurkuur_Voltijds-gewoon-secundair-onderwijs',
         children: [
           {
             label: 'Eerste graad',
-            value: 'cultuurkuur+AF8-eerste+AC0-graad',
+            value: 'cultuurkuur_eerste-graad',
             children: [
               {
-                label: 'A+AC0-stroom',
-                value: 'cultuurkuur+AF8-A+AC0-stroom',
+                label: 'A-stroom',
+                value: 'cultuurkuur_A-stroom',
               },
               {
-                label: 'B+AC0-stroom',
-                value: 'cultuurkuur+AF8-B+AC0-stroom',
+                label: 'B-stroom',
+                value: 'cultuurkuur_B-stroom',
               },
             ],
           },
           {
             label: 'Tweede graad',
-            value: 'cultuurkuur+AF8-tweede+AC0-graad',
+            value: 'cultuurkuur_tweede-graad',
             children: [
               {
                 label: 'Finaliteit doorstroom',
-                value: 'cultuurkuur+AF8-finaliteit-tweede+AC0-doorstroom',
+                value: 'cultuurkuur_finaliteit-tweede-doorstroom',
               },
               {
                 label: 'Finaliteit arbeidsmarkt',
-                value: 'cultuurkuur+AF8-finaliteit-tweede+AC0-arbeidsmarkt',
+                value: 'cultuurkuur_finaliteit-tweede-arbeidsmarkt',
               },
               {
                 label: 'Dubbele finaliteit',
-                value: 'cultuurkuur+AF8-dubbele+AC0-tweede-finaliteit',
+                value: 'cultuurkuur_dubbele-tweede-finaliteit',
               },
             ],
           },
           {
             label: 'Derde graad',
-            value: 'cultuurkuur+AF8-derde+AC0-graad',
+            value: 'cultuurkuur_derde-graad',
             children: [
               {
                 label: 'Finaliteit doorstroom',
-                value: 'cultuurkuur+AF8-finaliteit-derdre+AC0-doorstroom',
+                value: 'cultuurkuur_finaliteit-derdre-doorstroom',
               },
               {
                 label: 'Finaliteit arbeidsmarkt',
-                value: 'cultuurkuur+AF8-finaliteit-derdre+AC0-arbeidsmarkt',
+                value: 'cultuurkuur_finaliteit-derdre-arbeidsmarkt',
               },
               {
                 label: 'Dubbele finaliteit',
-                value: 'cultuurkuur+AF8-dubbele+AC0-derdre-finaliteit',
+                value: 'cultuurkuur_dubbele-derdre-finaliteit',
               },
             ],
           },
           {
-            label: 'Secundair na Secundair (Se+AC0-n+AC0-Se)',
-            value:
-              'cultuurkuur+AF8-Secundair+AC0-na+AC0-secundair+AC0-(Se+AC0-n+AC0-Se)',
+            label: 'Secundair na Secundair (Se-n-Se)',
+            value: 'cultuurkuur_Secundair-na-secundair-(Se-n-Se)',
           },
           {
             label: 'Onthaalonderwijs voor anderstalige nieuwkomers (OKAN)',
             value:
-              'cultuurkuur+AF8-Onthaalonderwijs+AC0-voor+AC0-anderstalige+AC0-nieuwkomers+AC0-OKAN',
+              'cultuurkuur_Onthaalonderwijs-voor-anderstalige-nieuwkomers-OKAN',
           },
         ],
       },
       {
         label: 'Buitengewoon secundair onderwijs',
-        value: 'cultuurkuur+AF8-Buitengewoon+AC0-secundair+AC0-onderwijs',
+        value: 'cultuurkuur_Buitengewoon-secundair-onderwijs',
       },
       {
         label: 'Deeltijds leren en werken',
-        value: 'cultuurkuur+AF8-Deeltijds+AC0-leren+AC0-en+AC0-werken',
+        value: 'cultuurkuur_Deeltijds-leren-en-werken',
       },
     ],
   },
   {
     label: 'Hoger onderwijs',
-    value: 'cultuurkuur+AF8-Hoger+AC0-onderwijs',
+    value: 'cultuurkuur_Hoger-onderwijs',
   },
   {
     label: 'Volwassenenonderwijs',
-    value: 'cultuurkuur+AF8-Volwassenenonderwijs',
+    value: 'cultuurkuur_Volwassenenonderwijs',
   },
   {
     label: 'Deeltijds kunstonderwijs',
-    value: 'cultuurkuur+AF8-Deeltijds+AC0-kunstonderwijs+AC0-DKO',
+    value: 'cultuurkuur_Deeltijds-kunstonderwijs-DKO',
     children: [
       {
         label: 'Beeldende en audiovisuele kunst',
-        value: 'cultuurkuur+AF8-Beeldende+AC0-en+AC0-audiovisuele+AC0-kunst',
+        value: 'cultuurkuur_Beeldende-en-audiovisuele-kunst',
       },
       {
         label: 'Dans',
-        value: 'cultuurkuur+AF8-dans',
+        value: 'cultuurkuur_dans',
       },
       {
         label: 'Muziek',
-        value: 'cultuurkuur+AF8-muziek',
+        value: 'cultuurkuur_muziek',
       },
       {
-        label: 'Woordkunst +ACY- drama',
-        value: 'cultuurkuur+AF8-Woordkunst+AC0-drama',
+        label: 'Woordkunst & drama',
+        value: 'cultuurkuur_Woordkunst-drama',
       },
     ],
   },
