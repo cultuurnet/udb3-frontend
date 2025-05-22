bcf77c597a2e46036b3548af7fa49184b4150398
vhande <hande.vanhove@publiq.be>
2025-05-22 11:07:06 +0200

    Fix lint

--- src/pages/steps/NameAndAgeRangeStep.tsx
+++ src/pages/steps/NameAndAgeRangeStep.tsx
@@ -1,5 +1,7 @@
 import { useRouter } from 'next/router';
+import { useMemo, useState } from 'react';
 import { Controller } from 'react-hook-form';
+import { useQueryClient } from 'react-query';
 import * as yup from 'yup';
 
 import { AudienceTypes } from '@/constants/AudienceType';
@@ -10,9 +12,12 @@ import {
   useChangeOfferNameMutation,
   useChangeOfferTypicalAgeRangeMutation,
 } from '@/hooks/api/offers';
+import { useGetEntityByIdAndScope } from '@/hooks/api/scope';
 import { FeatureFlags, useFeatureFlag } from '@/hooks/useFeatureFlag';
 import { CultuurkuurLabelsPicker } from '@/pages/steps/CultuurkuurLabelsPicker';
 import { isLocationSet } from '@/pages/steps/LocationStep';
+import { Offer } from '@/types/Offer';
+import { Organizer } from '@/types/Organizer';
 import { Place } from '@/types/Place';
 import { AlertVariants } from '@/ui/Alert';
 import { parseSpacing } from '@/ui/Box';
@@ -20,6 +25,7 @@ import { FormElement } from '@/ui/FormElement';
 import { LabelsCheckboxTree } from '@/ui/LabelsCheckboxTree';
 import { Stack } from '@/ui/Stack';
 import { DuplicatePlaceErrorBody } from '@/utils/fetchFromApi';
+import { getUniqueLabels } from '@/utils/getUniqueLabels';
 import { parseOfferId } from '@/utils/parseOfferId';
 
 import { AlertDuplicatePlace } from '../AlertDuplicatePlace';
@@ -32,12 +38,6 @@ import {
   StepProps,
   StepsConfiguration,
 } from './Steps';
-import { useMemo, useState } from 'react';
-import { getUniqueLabels } from '@/utils/getUniqueLabels';
-import { useGetEntityByIdAndScope } from '@/hooks/api/scope';
-import { Offer } from '@/types/Offer';
-import { Organizer } from '@/types/Organizer';
-import { useQueryClient } from 'react-query';
 
 const numberHyphenNumberRegex = /^(\d*-)?\d*$/;
 
