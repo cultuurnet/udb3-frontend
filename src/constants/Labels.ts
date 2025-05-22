0b5332ff6af3274c44b38b4fcc9e4a530e04577b
vhande <hande.vanhove@publiq.be>
2025-05-22 19:39:02 +0200

    Add constants Labels

---
+++ src/constants/Labels.ts
@@ -0,0 +1,4 @@
+const CULTUURKUUR_TAILORED_LABEL = 'cultuurkuur_op_maat';
+const CULTUURKUUR_ON_SITE_LABEL = 'cultuurkuur_op_verplaatsing';
+
+export { CULTUURKUUR_ON_SITE_LABEL, CULTUURKUUR_TAILORED_LABEL };
