d48b20d88f4f71e875de496ea000aa99747c3dc6
vhande <hande.vanhove@publiq.be>
2025-06-02 15:45:08 +0200

    Improve overview

--- src/pages/steps/CultuurkuurSelectionOverview.tsx
+++ src/pages/steps/CultuurkuurSelectionOverview.tsx
@@ -1,35 +1,58 @@
+import { useTranslation } from 'react-i18next';
+
 import { HierarchicalData } from '@/hooks/api/cultuurkuur';
-import { IconSuccess } from '@/ui/Alert';
-import { Box } from '@/ui/Box';
+import { Button } from '@/ui/Button';
 import { Inline } from '@/ui/Inline';
 
 type Props = {
   selectedData: HierarchicalData[];
+  labelsKey: string;
+  onOpenModal: () => void;
 };
 
-const CultuurkuurSelectionOverview = ({ selectedData }: Props) => {
+const CultuurkuurSelectionOverview = ({
+  selectedData,
+  labelsKey,
+  onOpenModal,
+}: Props) => {
+  const { t } = useTranslation();
+  const selectedCount = selectedData.length;
+
+  if (selectedCount === 0) return null;
+
   return (
-    <Box
-      css={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
-        gap: '0.25rem',
-        margin: '1rem 0',
-      }}
-    >
-      {selectedData.map((item) => (
-        <Inline padding={2} alignItems="center" key={item?.label}>
-          <IconSuccess
-            css={{
-              color: 'green',
-              marginRight: '0.5rem',
-              fontSize: '1rem',
-            }}
-          />
-          <span>{item?.name?.nl}</span>
-        </Inline>
-      ))}
-    </Box>
+    <Inline alignItems="center" marginTop={4} spacing={2}>
+      <p
+        css={`
+          margin: 0;
+        `}
+      >
+        <span
+          css={`
+            font-weight: bold;
+            margin-right: 4px;
+          `}
+        >
+          {selectedCount}
+        </span>
+        <span
+          css={`
+            font-weight: bold;
+            margin-right: 4px;
+          `}
+        >
+          {t(
+            labelsKey === 'location'
+              ? 'cultuurkuur_modal.overview.locations'
+              : 'cultuurkuur_modal.overview.education_levels',
+          )}
+        </span>
+        <span>{t('cultuurkuur_modal.overview.selected')}</span>
+      </p>
+      <Button variant="link" onClick={onOpenModal}>
+        {t('cultuurkuur_modal.overview.change')}
+      </Button>
+    </Inline>
   );
 };
 
