cd26c49ddc0dd46aaae6e4a984ffbe3062efbe65
vhande <hande.vanhove@publiq.be>
2025-06-03 09:59:22 +0200

    Add util displayCultuurkuurLabels

---
+++ src/utils/displayCultuurkuurLabels.ts
@@ -0,0 +1,16 @@
+import { PermissionTypes } from '@/constants/PermissionTypes';
+
+const displayCultuurkuurLabels = (roles: string[], labels: string[]) => {
+  const isGodUser = roles?.includes(PermissionTypes.GEBRUIKERS_BEHEREN);
+
+  const cultuurkuurLabels = labels.filter((label) =>
+    label.startsWith('cultuurkuur_'),
+  );
+  const otherLabels = labels.filter(
+    (label) => !label.startsWith('cultuurkuur_'),
+  );
+
+  return isGodUser ? [...cultuurkuurLabels, ...otherLabels] : [...otherLabels];
+};
+
+export { displayCultuurkuurLabels };
