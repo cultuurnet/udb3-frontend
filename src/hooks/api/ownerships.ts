9eaf819c4da130f240b2b0bb60ac7d391a826f76
vhande <hande.vanhove@publiq.be>
2025-06-12 16:15:18 +0200

    Update ownerships.ts

--- src/hooks/api/ownerships.ts
+++ src/hooks/api/ownerships.ts
@@ -150,7 +150,7 @@ const useGetOwnershipRequestsQuery = (
   return useAuthenticatedQuery({
     ...options,
     ...configuration,
-    enabled: options.enabled !== false && configuration.enabled !== false,
+    enabled: options.enabled !== false && !configuration.enabled
   });
 };
 
