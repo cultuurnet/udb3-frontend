2ddd8315cc28b65ce105d6efa035aacf8dc69f66
Bram Pauwelyn <brampauwelyn@users.noreply.github.com>
2025-09-02 09:20:41 +0200

    III-6354 - Fix typo

--- src/pages/_document.page.js
+++ src/pages/_document.page.js
@@ -49,7 +49,7 @@ class Document extends NextDocument {
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
             rel="stylesheet"
             integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-            crossorigin="anonymous"
+            crossOrigin="anonymous"
           />
         </Head>
         <body>
