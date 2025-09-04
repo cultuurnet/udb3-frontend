d240d5ed3091aec6e9a25ce918d4309f272806d3
Sander Vleugels <sandervleugels@gmail.com>
2025-09-04 13:39:37 +0200

    III-6756:  refactor the labels query functions to allow different query options and prefetch

--- src/hooks/api/labels.ts
+++ src/hooks/api/labels.ts
@@ -1,22 +1,44 @@
-import { useAuthenticatedQuery } from '@/hooks/api/authenticated-query';
+import {
+  PaginationOptions,
+  prefetchAuthenticatedQuery,
+  queryOptions,
+  ServerSideQueryOptions,
+  useAuthenticatedQuery,
+} from '@/hooks/api/authenticated-query';
 import type { Headers } from '@/hooks/api/types/Headers';
 import { Label } from '@/types/Offer';
 import { PaginatedData } from '@/types/PaginatedData';
 import { fetchFromApi, isErrorObject } from '@/utils/fetchFromApi';
 
-const getLabelsByQuery = async ({
+export const prefetchGetLabelsQuery = ({
+  req,
+  queryClient,
+  name,
+  paginationOptions,
+}: ServerSideQueryOptions & PaginationOptions & { name?: string }) =>
+  prefetchAuthenticatedQuery({
+    req,
+    queryClient,
+    ...createGetLabelsQueryOptions({ name, paginationOptions }),
+});
+
+const getLabels = async ({
   headers,
-  query,
+  name,
+  start,
+  limit
 }: {
   headers: Headers;
-  query: string;
+  name: string;
+  start: string;
+  limit: string;
 }) => {
   const res = await fetchFromApi({
     path: '/labels/',
     searchParams: {
-      query,
-      limit: '6',
-      start: '0',
+      query: name,
+      limit: limit,
+      start: start,
       suggestion: 'true',
     },
     options: {
@@ -26,12 +48,29 @@ const getLabelsByQuery = async ({
   return (await res.json()) as PaginatedData<Label[]>;
 };
 
-const useGetLabelsByQuery = ({ query }: { query: string }) =>
-  useAuthenticatedQuery({
-    queryKey: ['labels'],
-    queryFn: getLabelsByQuery,
-    queryArguments: { query },
-    enabled: !!query,
-  });
+const createGetLabelsQueryOptions = ({
+  name = '',
+  paginationOptions = { start: 0, limit: 10 }
+}: PaginationOptions & { name?: string}) => queryOptions({
+  queryKey: ['labels'],
+  queryFn: getLabels,
+  queryArguments: {
+    name,
+    start: `${paginationOptions.start}`,
+    limit: `${paginationOptions.limit}`,
+  }
+});
 
-export { getLabelsByQuery, useGetLabelsByQuery };
+const useGetLabelsByQuery = ({
+  name,
+  paginationOptions = { start: 0, limit: 10 }
+}: PaginationOptions & { name: string}) => {
+  const options = createGetLabelsQueryOptions({ name, paginationOptions });
+
+  return useAuthenticatedQuery({
+    ...options,
+  });
+}
+
+
+export { useGetLabelsByQuery };
