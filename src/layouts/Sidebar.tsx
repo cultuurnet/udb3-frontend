f6dae9b0ebba5d183c93ba5633ee0faae5fa1358
vhande <hande.vanhove@publiq.be>
2025-09-18 14:17:54 +0200

    Fix styling sidebar

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -105,6 +105,7 @@ const MenuItem = memo(
 
           :hover {
             background-color: ${getValueForMenuItem('hover.backgroundColor')};
+              border-radius: 8px;
           }
 
           :before {
