624fb765b90bfa6fb5c948989ed32cced1d5abf2
Sander Vleugels <sandervleugels@gmail.com>
2025-10-10 14:06:12 +0200

    Add e2e tests for the Labels overview page

---
+++ src/test/e2e/manage/labels/label-overview.admin.spec.ts
@@ -0,0 +1,28 @@
+import { expect, test } from '@playwright/test';
+
+test('can navigate to labels overview page', async ({ baseURL, page }) => {
+  await page.goto(baseURL + '/manage/labels');
+  await expect(page.getByRole('heading')).toContainText('Labels');
+});
+
+test('can search for labels', async ({ baseURL, page }) => {
+  await page.goto(baseURL + '/manage/labels');
+  await page.getByPlaceholder(/zoek/i).fill('test');
+  await page.waitForLoadState('networkidle');
+  await expect(page.getByPlaceholder(/zoek/i)).toHaveValue('test');
+});
+
+test('can click create label button', async ({ baseURL, page }) => {
+  await page.goto(baseURL + '/manage/labels');
+  await page.getByRole('button', { name: /toevoegen/i }).click();
+  await expect(page).toHaveURL(/\/manage\/labels\/create/);
+});
+
+test('can click edit label link', async ({ baseURL, page }) => {
+  await page.goto(baseURL + '/manage/labels');
+  const editLink = page.getByRole('link', { name: /bewerken/i }).first();
+  if (await editLink.isVisible()) {
+    await editLink.click();
+    await expect(page).toHaveURL(/\/manage\/labels\/[^/]+/);
+  }
+});
