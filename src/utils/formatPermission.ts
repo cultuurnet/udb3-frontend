706a3dbcd7dda8cc324afe06b01157e0ce1e9316
vhande <hande.vanhove@publiq.be>
2025-10-13 10:24:51 +0200

    Add comment

--- src/utils/formatPermission.ts
+++ src/utils/formatPermission.ts
@@ -1,3 +1,4 @@
+// Convert a string like EXAMPLE_PERMISSION into "Example permission" by replacing underscores with spaces, converting to lowercase, and capitalizing the first letter.
 const formatPermission = (permission: string) =>
   permission
     .replace(/_/g, ' ')
