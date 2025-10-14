438598af9db37ac19a2f020b3b1b545391c50644
Sander Vleugels <sandervleugels@gmail.com>
2025-10-14 12:20:46 +0200

    Set cookies before the Label overview tests

--- src/test/e2e/manage/labels/label-overview.admin.spec.ts
+++ src/test/e2e/manage/labels/label-overview.admin.spec.ts
@@ -1,28 +1,47 @@
 import { expect, test } from '@playwright/test';
 
-test('can navigate to labels overview page', async ({ baseURL, page }) => {
-  await page.goto(baseURL + '/manage/labels');
-  await expect(page.getByRole('heading')).toContainText('Labels');
-});
+test.describe('Label Overview - Admin', () => {
+  test.beforeEach(async ({ page, context }) => {
+    await context.addCookies([
+      {
+        name: 'ff_react_labels_create_edit',
+        value: 'true',
+        domain: 'localhost',
+        path: '/',
+      },
+      {
+        name: 'ff_react_labels_overview',
+        value: 'true',
+        domain: 'localhost',
+        path: '/',
+      },
+    ]);
+  });
 
-test('can search for labels', async ({ baseURL, page }) => {
-  await page.goto(baseURL + '/manage/labels');
-  await page.getByPlaceholder(/zoek/i).fill('test');
-  await page.waitForLoadState('networkidle');
-  await expect(page.getByPlaceholder(/zoek/i)).toHaveValue('test');
-});
+  test('can navigate to labels overview page', async ({ baseURL, page }) => {
+    await page.goto(baseURL + '/manage/labels');
+    await expect(page.getByRole('heading')).toContainText('Labels');
+  });
 
-test('can click create label button', async ({ baseURL, page }) => {
-  await page.goto(baseURL + '/manage/labels');
-  await page.getByRole('button', { name: /toevoegen/i }).click();
-  await expect(page).toHaveURL(/\/manage\/labels\/create/);
-});
+  test('can search for labels', async ({ baseURL, page }) => {
+    await page.goto(baseURL + '/manage/labels');
+    await page.getByPlaceholder(/zoek/i).fill('test');
+    await page.waitForLoadState('networkidle');
+    await expect(page.getByPlaceholder(/zoek/i)).toHaveValue('test');
+  });
 
-test('can click edit label link', async ({ baseURL, page }) => {
-  await page.goto(baseURL + '/manage/labels');
-  const editLink = page.getByRole('link', { name: /bewerken/i }).first();
-  if (await editLink.isVisible()) {
-    await editLink.click();
-    await expect(page).toHaveURL(/\/manage\/labels\/[^/]+\/edit/);
-  }
+  test('can click create label button', async ({ baseURL, page }) => {
+    await page.goto(baseURL + '/manage/labels');
+    await page.getByRole('button', { name: /toevoegen/i }).click();
+    await expect(page).toHaveURL(/\/manage\/labels\/create/);
+  });
+
+  test('can click edit label link', async ({ baseURL, page }) => {
+    await page.goto(baseURL + '/manage/labels');
+    const editLink = page.getByRole('link', { name: /bewerken/i }).first();
+    if (await editLink.isVisible()) {
+      await editLink.click();
+      await expect(page).toHaveURL(/\/manage\/labels\/[^/]+\/edit/);
+    }
+  });
 });
