3b92d409cd657da899396f7250345f12c5061829
Bram Pauwelyn <brampauwelyn@users.noreply.github.com>
2025-11-17 10:23:20 +0100

    III-6767 - Progress on translation UI

--- src/pages/events/[eventId]/translate/TranslateForm.tsx
+++ src/pages/events/[eventId]/translate/TranslateForm.tsx
@@ -1,7 +1,13 @@
 import { useRouter } from 'next/router';
 
-import { useGetEventByIdQuery } from '@/hooks/api/events';
+import {
+  useChangeNameMutation,
+  useGetEventByIdQuery,
+} from '@/hooks/api/events';
+import { useChangeDescriptionMutation } from '@/hooks/api/offers';
 import { SupportedLanguages } from '@/i18n/index';
+import { Button } from '@/ui/Button';
+import { Inline } from '@/ui/Inline';
 import { Page } from '@/ui/Page';
 import { Stack } from '@/ui/Stack';
 
@@ -16,17 +22,57 @@ const TranslateForm = () => {
 
   console.log('Event to translate:', event);
 
-  console.log('event name', event?.name);
+  const onDescriptionSuccess = () => {
+    console.log('Description changed successfully');
+  };
+
+  const onNameSuccess = () => {
+    console.log('Name changed successfully');
+  };
+
+  const changeDescriptionMutation = useChangeDescriptionMutation({
+    onSuccess: onDescriptionSuccess,
+  });
+
+  const changeNameMutation = useChangeNameMutation({
+    onSuccess: onNameSuccess,
+  });
 
   return (
     <Page>
       <Page.Title>Vertaal</Page.Title>
       <Page.Content>
-        <Stack>
-          {Object.entries(SupportedLanguages).map(([lang]) => (
-            <input key={lang} placeholder={`Title in ${lang}`} />
-          ))}
+        <Stack backgroundColor="white" padding={4} spacing={4}>
+          <Inline>
+            <p>
+              <strong>Titel:</strong> Hello world
+            </p>
+          </Inline>
+          <Inline>
+            {Object.entries(SupportedLanguages).map(([lang]) => (
+              <input
+                disabled={lang.toLowerCase() === event.mainLanguage}
+                key={lang}
+                placeholder={`Title in ${lang}`}
+              />
+            ))}
+          </Inline>
+          <Inline>
+            <p>Omschrijving</p>
+          </Inline>
+          <Inline>
+            {Object.entries(SupportedLanguages).map(([lang]) => (
+              <input
+                disabled={lang.toLowerCase() === event.mainLanguage}
+                key={lang}
+                placeholder={`Description in ${lang}`}
+              />
+            ))}
+          </Inline>
         </Stack>
+        <Inline>
+          <Button variant="success">Klaar met vertalen</Button>
+        </Inline>
       </Page.Content>
     </Page>
   );
