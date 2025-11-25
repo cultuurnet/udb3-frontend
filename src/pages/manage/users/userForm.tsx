7dcddb58514626117e18cd58f525360eaa81267c
Bram Pauwelyn <brampauwelyn@users.noreply.github.com>
2025-11-25 14:11:08 +0100

    III-6765 - Remove console.log

--- src/pages/manage/users/userForm.tsx
+++ src/pages/manage/users/userForm.tsx
@@ -3,7 +3,6 @@ import { Alert } from 'react-bootstrap';
 import { User, UserById } from '@/hooks/api/user';
 
 const UserForm = ({ user }: { user: User | UserById | undefined }) => {
-  console.log(user);
   return (
     <Alert>{user ? `Editing user: ${user.email}` : 'Loading user...'}</Alert>
   );
