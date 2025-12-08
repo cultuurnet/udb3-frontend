ec8b098e3d92f08bef8566d1bfb156fa3396cf11
Bram Pauwelyn <brampauwelyn@users.noreply.github.com>
2025-12-08 11:31:40 +0100

    III-6931 - Fetch offer detail

--- src/pages/events/[eventId]/preview/Preview.tsx
+++ src/pages/events/[eventId]/preview/Preview.tsx
@@ -1,9 +1,23 @@
+import { useRouter } from 'next/router';
+
+import { OfferTypes } from '@/constants/OfferType';
+import { useGetOfferByIdQuery } from '@/hooks/api/offers';
 import { Page } from '@/ui/Page';
 
 const Preview = () => {
+  const router = useRouter();
+  const { eventId } = router.query;
+
+  const getOfferByIdQuery = useGetOfferByIdQuery({
+    id: eventId as string,
+    scope: OfferTypes.EVENTS,
+  });
+
+  const title = getOfferByIdQuery.data?.name.nl;
+
   return (
     <Page>
-      <Page.Title>Event Preview</Page.Title>
+      <Page.Title>{title}</Page.Title>
     </Page>
   );
 };
