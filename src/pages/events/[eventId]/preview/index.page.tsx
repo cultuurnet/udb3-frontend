68ad9f5cc007f8299e36a82f5aa7508c277f14c3
Bram Pauwelyn <brampauwelyn@users.noreply.github.com>
2025-12-08 11:31:19 +0100

    III-6931 - Prefetch event detail on server

--- src/pages/events/[eventId]/preview/index.page.tsx
+++ src/pages/events/[eventId]/preview/index.page.tsx
@@ -1,11 +1,25 @@
+import { dehydrate } from '@tanstack/react-query';
+
+import { OfferTypes } from '@/constants/OfferType';
+import { prefetchGetOfferByIdQuery } from '@/hooks/api/offers';
 import { getApplicationServerSideProps } from '@/utils/getApplicationServerSideProps';
 
 import { Preview } from './Preview';
 
 export const getServerSideProps = getApplicationServerSideProps(
-  async ({ cookies }) => {
+  async ({ cookies, query, req, queryClient }) => {
+    const { eventId } = query;
+
+    await prefetchGetOfferByIdQuery({
+      req,
+      queryClient,
+      id: eventId as string,
+      scope: OfferTypes.EVENTS,
+    });
+
     return {
       props: {
+        dehydratedState: dehydrate(queryClient),
         cookies,
       },
     };
