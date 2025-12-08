1a5e2475a88c845e3c4b518f367358be4be6217e
Bram Pauwelyn <brampauwelyn@users.noreply.github.com>
2025-12-08 13:30:45 +0100

    III-6931 - Add tab table content

--- src/pages/events/[eventId]/preview/Preview.tsx
+++ src/pages/events/[eventId]/preview/Preview.tsx
@@ -2,8 +2,15 @@ import { useRouter } from 'next/router';
 
 import { OfferTypes } from '@/constants/OfferType';
 import { useGetOfferByIdQuery } from '@/hooks/api/offers';
-import i18n from '@/i18n/index';
+import i18n, { SupportedLanguage } from '@/i18n/index';
 import { Page } from '@/ui/Page';
+import { Stack } from '@/ui/Stack';
+import { Table } from '@/ui/Table';
+import { Tabs } from '@/ui/Tabs';
+import { getGlobalBorderRadius, getValueFromTheme } from '@/ui/theme';
+import { getLanguageObjectOrFallback } from '@/utils/getLanguageObjectOrFallback';
+
+const getGlobalValue = getValueFromTheme('global');
 
 const Preview = () => {
   const router = useRouter();
@@ -16,14 +23,59 @@ const Preview = () => {
 
   const offer = getOfferByIdQuery.data;
 
-  const title =
-    offer?.name?.[i18n.language] ??
-    offer?.name?.[offer.mainLanguage] ??
-    offer?.name;
+  const { mainLanguage } = offer;
+
+  const title = getLanguageObjectOrFallback<string>(
+    offer?.name,
+    i18n.language as SupportedLanguage,
+    mainLanguage,
+  );
+
+  const tabOptions = ['details'];
+
+  const activeTab = 'details';
+
+  const onTabChange = (key: string) => {
+    console.log('tab changed to:', key);
+  };
+
+  const columns = [
+    { Header: 'Field', accessor: 'field' },
+    { Header: 'Value', accessor: 'value' },
+  ];
+
+  const tableData = [{ field: 'Titel', value: title }];
 
   return (
     <Page>
       <Page.Title>{title}</Page.Title>
+      <Page.Content>
+        <Tabs
+          activeKey={activeTab}
+          onSelect={(key) => onTabChange(key as string)}
+        >
+          {tabOptions.map((tab) => (
+            <Tabs.Tab eventKey={tab} title={'Gegevens'}>
+              <Stack
+                marginTop={4}
+                backgroundColor="white"
+                padding={4}
+                borderRadius={getGlobalBorderRadius}
+                css={`
+                  box-shadow: ${getGlobalValue('boxShadow.medium')};
+                `}
+              >
+                <Table
+                  bordered
+                  showHeader={false}
+                  columns={columns}
+                  data={tableData}
+                />
+              </Stack>
+            </Tabs.Tab>
+          ))}
+        </Tabs>
+      </Page.Content>
     </Page>
   );
 };
