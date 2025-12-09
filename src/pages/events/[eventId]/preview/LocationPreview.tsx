3862401f212ab9ea2d745502a95db738357fdc23
Bram Pauwelyn <brampauwelyn@users.noreply.github.com>
2025-12-09 16:33:25 +0100

    III-6931 - Handle online case

--- src/pages/events/[eventId]/preview/LocationPreview.tsx
+++ src/pages/events/[eventId]/preview/LocationPreview.tsx
@@ -1,8 +1,12 @@
+import { useTranslation } from 'react-i18next';
+
+import { ONLINE_LOCATION_ID } from '@/constants/Location';
 import i18n, { SupportedLanguage } from '@/i18n/index';
 import { AddressInternal } from '@/types/Address';
 import { Offer } from '@/types/Offer';
 import { isPlace } from '@/types/Place';
 import { Link } from '@/ui/Link';
+import { Text } from '@/ui/Text';
 import { getLanguageObjectOrFallback } from '@/utils/getLanguageObjectOrFallback';
 import { parseOfferId } from '@/utils/parseOfferId';
 
@@ -11,11 +15,19 @@ type Props = {
 };
 
 const LocationPreview = ({ offer }: Props) => {
+  const { t } = useTranslation();
+
   if (isPlace(offer)) return null;
 
   const { location, mainLanguage } = offer;
 
   const locationId = parseOfferId(location['@id']);
+
+  const isOnlineLocation = locationId === ONLINE_LOCATION_ID;
+
+  if (isOnlineLocation) {
+    return <Text>{t('preview.online_label')}</Text>;
+  }
 
   const locationName = getLanguageObjectOrFallback<string>(
     location.name,
