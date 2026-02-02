5b41c55119b022b533f34f0d70ab745f702947dc
Sander Vleugels <sandervleugels@gmail.com>
2026-02-02 12:14:54 +0100

    III-6978: add theme preview with translations

--- src/pages/events/[eventId]/preview/Preview.tsx
+++ src/pages/events/[eventId]/preview/Preview.tsx
@@ -69,6 +69,7 @@ const Preview = () => {
   );
 
   const typeTerm = terms.find((term) => term.domain === 'eventtype');
+  const themeTerm = terms.find((term) => term.domain === 'theme');
 
   const description = getLanguageObjectOrFallback<string>(
     offer.description,
@@ -271,6 +272,14 @@ const Preview = () => {
   const tableData = [
     { field: t('preview.labels.title'), value: title },
     { field: t('preview.labels.type'), value: typeTerm.label },
+    {
+      field: t('preview.labels.theme'),
+      value: themeTerm ? (
+        themeTerm.label
+      ) : (
+        <EmptyValue>{t('preview.empty_value.theme')}</EmptyValue>
+      ),
+    },
     {
       field: t('preview.labels.labels'),
       value: (
