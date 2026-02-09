d0c5f500df9f88486294b25ac88ca1ed3402552e
Sander Vleugels <sandervleugels@gmail.com>
2026-02-09 08:32:02 +0100

    III-7028: add feature flag

--- src/hooks/useFeatureFlag.ts
+++ src/hooks/useFeatureFlag.ts
@@ -17,6 +17,7 @@ const FeatureFlags = {
   REACT_DUPLICATE: 'react_duplicate',
   REACT_LABELS_CREATE_EDIT: 'react_labels_create_edit',
   SHOW_CONSOLE_DEBUGGING: 'show_console_debugging',
+  SHOW_IFRAME_BORDER: 'show_iframe_border',
   REACT_ROLES_OVERVIEW: 'react_roles_overview',
   REACT_ROLES_CREATE_EDIT: 'react_roles_create_edit',
   REACT_USERS_SEARCH: 'react_users_search',
