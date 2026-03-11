4ca63fa2ddfcd1cc00409df73eac64d730089dca
vhande <78013271+vhande@users.noreply.github.com>
2026-03-11 13:20:25 +0100

    Add place preview

---
+++ src/pages/places/[placeId]/index.page.tsx
@@ -0,0 +1,55 @@
+import { dehydrate } from '@tanstack/react-query';
+
+import { OfferContexts, OfferTypes } from '@/constants/OfferType';
+import { prefetchGetOfferPermissionsQuery } from '@/hooks/api/events';
+import {
+  prefetchGetOfferByIdQuery,
+  prefetchOfferHistoryQuery,
+} from '@/hooks/api/offers';
+import { prefetchGetPermissionsQuery } from '@/hooks/api/user';
+import { getApplicationServerSideProps } from '@/utils/getApplicationServerSideProps';
+
+import { Preview } from './Preview';
+
+export const getServerSideProps = getApplicationServerSideProps(
+  async ({ cookies, query, req, queryClient }) => {
+    const { placeId } = query;
+
+    await prefetchGetOfferByIdQuery({
+      req,
+      queryClient,
+      id: placeId as string,
+      scope: OfferTypes.PLACES,
+    });
+
+    await prefetchGetPermissionsQuery({
+      req,
+      queryClient,
+    });
+
+    await prefetchGetOfferPermissionsQuery({
+      req,
+      queryClient,
+      offerId: placeId,
+      offerType: OfferContexts.PLACES,
+    });
+
+    if (query.tab === 'history') {
+      await prefetchOfferHistoryQuery({
+        req,
+        queryClient,
+        id: placeId as string,
+        scope: OfferTypes.PLACES,
+      });
+    }
+
+    return {
+      props: {
+        dehydratedState: dehydrate(queryClient),
+        cookies,
+      },
+    };
+  },
+);
+
+export default Preview;
