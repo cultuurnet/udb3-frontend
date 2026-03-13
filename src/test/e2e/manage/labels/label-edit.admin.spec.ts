d845d89ddd2891c6226e0e75c7b0854f315fff1e
vhande <78013271+vhande@users.noreply.github.com>
2026-03-13 11:26:26 +0100

    Remove ff e2e tests

--- src/test/e2e/manage/labels/label-edit.admin.spec.ts
+++ src/test/e2e/manage/labels/label-edit.admin.spec.ts
@@ -2,14 +2,6 @@ import { expect, test } from '@playwright/test';
 
 test.describe('Label Editing - Admin', () => {
   test.beforeEach(async ({ page, context }) => {
-    await context.addCookies([
-      {
-        name: 'ff_react_labels_create_edit',
-        value: 'true',
-        domain: 'localhost',
-        path: '/',
-      },
-    ]);
     await page.goto('/manage/labels/324920da-385d-4adc-8cc1-e03e0cde8836/edit');
   });
 
