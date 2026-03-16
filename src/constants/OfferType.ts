139a9b2c70306e09dc802e40b37e10a33c64c36a
vhande <78013271+vhande@users.noreply.github.com>
2026-03-16 11:10:57 +0100

    Remove OfferContexts

--- src/constants/OfferType.ts
+++ src/constants/OfferType.ts
@@ -5,16 +5,10 @@ const OfferTypes = {
   PLACES: 'places',
 } as const;
 
-const OfferContexts = {
-  EVENT: 'event',
-  PLACES: 'place',
-} as const;
-
 const ScopeTypes = { ...OfferTypes, ORGANIZERS: 'organizers' } as const;
 
 type OfferType = Values<typeof OfferTypes>;
-type OfferContext = Values<typeof OfferContexts>;
 type Scope = Values<typeof ScopeTypes>;
 
-export type { OfferContext, OfferType, Scope };
-export { OfferContexts, OfferTypes, ScopeTypes };
+export type { OfferType, Scope };
+export { OfferTypes, ScopeTypes };
