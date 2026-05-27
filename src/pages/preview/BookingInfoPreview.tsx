9a8af1df208cbb2004416bde77d4b528c420fd90
vhande <78013271+vhande@users.noreply.github.com>
2026-05-27 14:03:02 +0200

    Update styling BookingInfoPreview

--- src/pages/preview/BookingInfoPreview.tsx
+++ src/pages/preview/BookingInfoPreview.tsx
@@ -5,6 +5,7 @@ import { BookingInfo, Offer } from '@/types/Offer';
 import { Link, LinkVariants } from '@/ui/Link';
 import { Stack } from '@/ui/Stack';
 import { Text } from '@/ui/Text';
+import { colors } from '@/ui/theme';
 import { formatPeriod } from '@/utils/formatPeriod';
 import { getLanguageObjectOrFallback } from '@/utils/getLanguageObjectOrFallback';
 
@@ -25,51 +26,43 @@ const BookingInfoPreview = ({ bookingInfo, mainLanguage }: Props) => {
     );
   }
 
-  return (
-    <Stack spacing={3}>
-      <Stack marginBottom={4} flexDirection="row" alignItems="stretch">
-        <Stack width="50%">
-          {hasBookingInfo && (
-            <Text fontWeight="600" marginBottom={3}>
-              {t('preview.booking_data')}
-            </Text>
-          )}
-          {bookingInfo.email && (
-            <Link href={`mailto:${bookingInfo.email}`}>
-              {bookingInfo.email}
-            </Link>
-          )}
-          {bookingInfo.phone && <Text>{bookingInfo.phone}</Text>}
-          {bookingInfo.url && (
-            <Link target="_blank" href={bookingInfo.url}>
-              {bookingInfo.url}
-            </Link>
-          )}
+  const rows = [
+    bookingInfo.email && {
+      label: t('create.additionalInformation.booking_info.email'),
+      value: (
+        <Link href={`mailto:${bookingInfo.email}`}>{bookingInfo.email}</Link>
+      ),
+    },
+    bookingInfo.phone && {
+      label: t('create.additionalInformation.booking_info.phone'),
+      value: <Text>{bookingInfo.phone}</Text>,
+    },
+    bookingInfo.url && {
+      label: t('preview.booking_link_label'),
+      value: (
+        <Stack spacing={2}>
+          <Link target="_blank" href={bookingInfo.url}>
+            {bookingInfo.url}
+          </Link>
+          <Link
+            variant={LinkVariants.BUTTON_PRIMARY}
+            target="_blank"
+            href={bookingInfo.url}
+            alignSelf="flex-start"
+          >
+            {getLanguageObjectOrFallback(
+              bookingInfo.urlLabel,
+              i18n.language as SupportedLanguage,
+              mainLanguage,
+            )}
+          </Link>
         </Stack>
-        {bookingInfo.url && (
-          <Stack>
-            <Text fontWeight="600" marginBottom={3}>
-              {t('preview.booking_url')}
-            </Text>
-            <Link
-              variant={LinkVariants.BUTTON_PRIMARY}
-              target="_blank"
-              href={bookingInfo.url}
-            >
-              {getLanguageObjectOrFallback(
-                bookingInfo.urlLabel,
-                i18n.language as SupportedLanguage,
-                mainLanguage,
-              )}
-            </Link>
-          </Stack>
-        )}
-      </Stack>
-      {bookingInfo.availabilityStarts && bookingInfo.availabilityEnds && (
-        <Stack>
-          <Text fontWeight="600" marginBottom={3}>
-            {t('preview.booking_period')}
-          </Text>
+      ),
+    },
+    bookingInfo.availabilityStarts &&
+      bookingInfo.availabilityEnds && {
+        label: t('preview.booking_period'),
+        value: (
           <Text>
             {formatPeriod(
               bookingInfo.availabilityStarts,
@@ -78,9 +71,39 @@ const BookingInfoPreview = ({ bookingInfo, mainLanguage }: Props) => {
               t,
             )}
           </Text>
-        </Stack>
-      )}
-    </Stack>
+        ),
+      },
+  ].filter(Boolean);
+
+  return (
+    <table
+      css={`
+        width: 100%;
+
+        td {
+          padding-left: 0 !important;
+          border-bottom: 1px solid ${colors.grey3};
+        }
+        tr:first-child td {
+          padding-top: 0;
+        }
+        td:first-child {
+          color: ${colors.grey5};
+        }
+        tr:last-child td {
+          border-bottom: none;
+        }
+      `}
+    >
+      <tbody>
+        {rows.map((row, index) => (
+          <tr key={index}>
+            <td>{row.label}</td>
+            <td>{row.value}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
