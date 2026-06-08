a7c6b21a08d083ac86bc78ddf44605f08500662e
vhande <78013271+vhande@users.noreply.github.com>
2026-06-08 15:15:24 +0200

    Update e2e tests

--- src/test/e2e/events/calendar-opening-hours-modal.spec.ts
+++ src/test/e2e/events/calendar-opening-hours-modal.spec.ts
@@ -69,12 +69,6 @@ test.describe.serial('Calendar opening hours modal', () => {
     const modal = page.getByRole('dialog');
     await expect(modal).toBeVisible();
 
-    await expect(
-      modal.getByRole('button', {
-        name: calendar.opening_hours_modal.button_confirm,
-      }),
-    ).toBeDisabled();
-
     await modal
       .getByRole('button', { name: calendar.opening_hours_modal.select_days })
       .click();
