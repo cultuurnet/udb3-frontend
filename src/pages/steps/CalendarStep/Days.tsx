c7819315ccf3e34c8078471303620bcf4990f427
vhande <78013271+vhande@users.noreply.github.com>
2026-06-11 09:25:18 +0200

    Fix lint

--- src/pages/steps/CalendarStep/Days.tsx
+++ src/pages/steps/CalendarStep/Days.tsx
@@ -16,7 +16,7 @@ import { Label, LabelVariants } from '@/ui/Label';
 import { List } from '@/ui/List';
 import { RadioButton, RadioButtonTypes } from '@/ui/RadioButton';
 import { getStackProps, Stack, StackProps } from '@/ui/Stack';
-import { Text, TextVariants } from '@/ui/Text';
+import { Text } from '@/ui/Text';
 import { colors } from '@/ui/theme';
 import {
   TimeSpanPicker,
