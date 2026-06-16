90dba6679de8e182487ba322db5ce2fb5e0290f4
Daniiel Mykytiuk <dani4ku@gmail.com>
2026-06-16 10:27:35 +0200

    set width to 600 in storybook

--- src/ui/Image.stories.tsx
+++ src/ui/Image.stories.tsx
@@ -27,5 +27,6 @@ export const Default: Story = {
   args: {
     src: 'https://images.unsplash.com/photo-1605460504109-f347ec557483?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
     alt: 'Sample image',
+    width: 600,
   },
 };
