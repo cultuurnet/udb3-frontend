1e5b3b6d14a19d77ed840bb0f294f64ed56b577e
vhande <78013271+vhande@users.noreply.github.com>
2026-07-01 10:41:15 +0200

    Create sortPeriods.ts

---
+++ src/pages/steps/CalendarStep/sortPeriods.ts
@@ -0,0 +1,8 @@
+import type { ClosingPeriodData } from './ClosingPeriod';
+import type { DeviatingPeriodData } from './DeviatingPeriod';
+
+const sortPeriods = (
+  items: ClosingPeriodData[] | DeviatingPeriodData[],
+) => [...items].sort((a, b) => a.startDate.getTime() - b.startDate.getTime());
+
+export { sortPeriods };
