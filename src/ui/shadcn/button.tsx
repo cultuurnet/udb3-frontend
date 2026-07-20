c987648d6df9d1b5d64a9880d3da0e59319d38fa
Daniiel Mykytiuk <dani4ku@gmail.com>
2026-07-20 11:53:34 +0200

    Match Button's sm size height to default

--- src/ui/shadcn/button.tsx
+++ src/ui/shadcn/button.tsx
@@ -34,7 +34,7 @@ const buttonVariants = cva(
       },
       size: {
         default: 'tw:h-10 tw:px-4 tw:py-2',
-        sm: 'tw:h-8 tw:rounded-md tw:px-3 tw:text-xs',
+        sm: 'tw:h-10 tw:rounded-md tw:px-3 tw:text-xs',
         lg: 'tw:h-10 tw:rounded-md tw:px-8',
         icon: 'tw:h-9 tw:w-9',
       },
