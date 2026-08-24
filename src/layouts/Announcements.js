c74dbafb3d85638052b1eb9e4d006fbb046a921c
Daniiel Mykytiuk <dani4ku@gmail.com>
2026-08-24 15:55:22 +0200

    Support urlSuffix object shape in announcement callToAction (unconfirmed guess, pending CMS fix)

--- src/layouts/Announcements.js
+++ src/layouts/Announcements.js
@@ -80,6 +80,14 @@ Announcement.propTypes = {
   onClick: PropTypes.func,
 };
 
+const getCallToActionHref = (callToAction) => {
+  if (typeof callToAction === 'string' && callToAction) return callToAction;
+  if (callToAction && typeof callToAction.urlSuffix === 'string') {
+    return callToAction.urlSuffix || null;
+  }
+  return null;
+};
+
 const AnnouncementContent = ({
   title,
   imageSrc,
@@ -87,8 +95,8 @@ const AnnouncementContent = ({
   callToAction,
   callToActionLabel,
 }) => {
-  const hasCallToAction =
-    typeof callToAction === 'string' && callToAction.length > 0;
+  const callToActionHref = getCallToActionHref(callToAction);
+  const hasCallToAction = !!callToActionHref;
 
   const image = (
     <Image
@@ -105,7 +113,7 @@ const AnnouncementContent = ({
     <Stack as="article" padding={4} spacing={3} width="70%">
       <Title>{title}</Title>
       {!!imageSrc && hasCallToAction ? (
-        <Link href={callToAction}>{image}</Link>
+        <Link href={callToActionHref}>{image}</Link>
       ) : (
         image
       )}
@@ -146,7 +154,7 @@ const AnnouncementContent = ({
       />
       <Inline as="div" justifyContent="flex-end">
         {hasCallToAction && (
-          <Link href={callToAction} variant={LinkVariants.BUTTON_PRIMARY}>
+          <Link href={callToActionHref} variant={LinkVariants.BUTTON_PRIMARY}>
             <Text>{callToActionLabel}</Text>
           </Link>
         )}
@@ -159,7 +167,10 @@ AnnouncementContent.propTypes = {
   title: PropTypes.string,
   imageSrc: PropTypes.string,
   body: PropTypes.node,
-  callToAction: PropTypes.string,
+  callToAction: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.shape({ urlSuffix: PropTypes.string }),
+  ]),
   callToActionLabel: PropTypes.string,
 };
 
