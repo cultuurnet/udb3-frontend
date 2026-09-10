7b854e522a67bf262f6d6ffc29f594ff412203b7
vhande <78013271+vhande@users.noreply.github.com>
2026-09-10 12:47:57 +0200

    Update switch

--- src/ui/SwitchLegacy.tsx
+++ src/ui/SwitchLegacy.tsx
@@ -18,7 +18,7 @@ const SwitchLegacy = ({
   checked = false,
   disabled = false,
   onCheckedChange = () => {},
-  color = colors.udbMainBlue,
+  color = colors.brandAccessible,
   className,
   'aria-label': ariaLabel,
 }: SwitchLegacyProps) => (
