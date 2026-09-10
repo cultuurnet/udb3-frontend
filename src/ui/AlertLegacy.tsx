e12590208d558cb3053bfc4bf99acb1969b16aaa
vhande <78013271+vhande@users.noreply.github.com>
2026-09-10 12:23:03 +0200

    Update colors: Alert

--- src/ui/AlertLegacy.tsx
+++ src/ui/AlertLegacy.tsx
@@ -8,7 +8,7 @@ import type { InlineProps } from './Inline';
 import { Inline } from './Inline';
 import { getStackProps, Stack } from './Stack';
 import { Text } from './Text';
-import { getValueFromTheme } from './theme';
+import { colors, getValueFromTheme } from './theme';
 
 const IconWarning = () => {
   return (
@@ -22,7 +22,7 @@ const IconWarning = () => {
         <g clipPath="url(#b)">
           <path
             d="M1 21h22L12 2 1 21Zm12-3h-2v-2h2v2Zm0-4h-2v-4h2v4Z"
-            fill="#E69336"
+            fill={colors.warningIcon}
           />
         </g>
       </g>
@@ -49,7 +49,7 @@ export const IconSuccess = () => {
       <g clipPath="url(#a)">
         <path
           d="M9 16.2 4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2Z"
-          fill="#6BCD69"
+          fill={colors.udbMainPositiveGreen}
         />
       </g>
       <defs>
@@ -71,7 +71,7 @@ const IconInfo = () => {
     >
       <path
         d="M14 14V9c0-3.07-1.64-5.64-4.5-6.32V2C9.5 1.17 8.83.5 8 .5S6.5 1.17 6.5 2v.68C3.63 3.36 2 5.92 2 9v5l-2 2v1h16v-1l-2-2Zm-5 0H7v-2h2v2Zm0-4H7V6h2v4ZM8 20c1.1 0 2-.9 2-2H6a2 2 0 0 0 2 2Z"
-        fill="#3868EC"
+        fill={colors.info}
       />
     </svg>
   );
@@ -88,7 +88,7 @@ const IconError = () => {
       <g clipPath="url(#a)">
         <path
           d="M11 15h2v2h-2v-2Zm0-8h2v6h-2V7Zm.99-5C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2ZM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8Z"
-          fill="#DD5242"
+          fill={colors.danger}
         />
       </g>
       <defs>
