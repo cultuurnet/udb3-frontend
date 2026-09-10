1f4f22b52862ab541a74376e62c60bcf596c9fed
Daniiel Mykytiuk <dani4ku@gmail.com>
2026-09-10 14:01:00 +0200

    cover the 500 page in page-level VRT

--- src/test/vrt-pages/static.spec.ts
+++ src/test/vrt-pages/static.spec.ts
@@ -4,5 +4,6 @@ screenshotPages([
   { title: 'copyright', path: '/copyright' },
   { title: 'unauthorized', path: '/unauthorized' },
   { title: '404', path: '/404', locator: (page) => page.locator('body') },
+  { title: '500', path: '/500', locator: (page) => page.locator('body') },
   { title: 'login', path: '/login', locator: (page) => page.locator('body') },
 ]);
